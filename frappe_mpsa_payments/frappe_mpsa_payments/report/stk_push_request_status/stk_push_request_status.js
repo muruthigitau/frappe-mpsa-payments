8209e22c0698dcd0a73279657ea2d960a28f377f
symonmuchemi <simonkenya846@gmail.com>
2025-06-15 23:54:23 +0300

    feat: add filters for STK Push Request Status report
    Added new filters to the STK Push Request Status report, including 'status', 'start_date', 'end_date', and 'phone_number'. These filters allow users to refine their queries based on transaction status, date range, and optional phone number input, enhancing the report's usability and functionality.

--- frappe_mpsa_payments/frappe_mpsa_payments/report/stk_push_request_status/stk_push_request_status.js
+++ frappe_mpsa_payments/frappe_mpsa_payments/report/stk_push_request_status/stk_push_request_status.js
@@ -3,6 +3,31 @@
 
 frappe.query_reports["STK Push Request Status"] = {
 	"filters": [
-
+		{
+			"fieldname": "status",
+			"label": __("Status"),
+			"fieldtype": "Select",
+			"options": "\nPending\nSuccess\nFailed",
+			"default": "Pending",
+		},
+		{
+			"fieldname": "start_date",
+			"label": __("Start Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_days(frappe.datetime.get_today(), -30),
+		}, 
+		{
+			"fieldname": "end_date",
+			"label": __("End Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+		},
+		{
+			"fieldname": "phone_number",
+			"label": __("Phone Number"),
+			"fieldtype": "Data",
+			"description": __("Filter by phone number (optional)"),
+			"default": "",
+		},
 	]
 };
