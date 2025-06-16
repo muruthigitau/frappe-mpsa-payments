86c30211747aa8c6df92970d5cfb66f643191808
Emmanuel Mwendwa <mwendwae054@gmail.com>
2025-06-16 15:27:42 +0200

    feat: adds loan disbursement link to B2C payments
    Introduces linking of loan disbursement records to B2C payment disbursements, updates related document references, and enhances the workspace to include Loan Disbursement for improved tracking and reconciliation of funds.

--- frappe_mpsa_payments/frappe_mpsa_payments/api/loan_disbursement.py
+++ frappe_mpsa_payments/frappe_mpsa_payments/api/loan_disbursement.py
@@ -14,6 +14,10 @@ def create_loan_disbursement(b2c_disbursement, b2c_disbursement_ref):
             pending_amount=b2c_disbursement_ref.allocated_amount
         )
 
+        disbursement_entry.reference_date = b2c_disbursement_ref.reference_date
+        disbursement_entry.reference_number = b2c_disbursement_ref.reference_no
+        disbursement_entry.b2c_payment_disbursement = b2c_disbursement.name
+
         disbursement_entry.insert(ignore_permissions=True)
         # disbursement_entry.submit()
 
