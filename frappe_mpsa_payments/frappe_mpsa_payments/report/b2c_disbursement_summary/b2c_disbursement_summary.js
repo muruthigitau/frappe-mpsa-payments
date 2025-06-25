21b025f52e1a6aff8a2c77405c34693d878b3770
symonmuchemi <simonkenya846@gmail.com>
2025-06-26 02:11:45 +0300

    feat: add custom formatter for B2C Disbursement Summary report
    Implemented a custom formatter function to enhance the display of values in the B2C Disbursement Summary report. The formatter checks for subtotal rows and applies bold styling, while also handling blank fields for the 'total_amount' column. This ensures that empty or zero values are displayed correctly, improving the overall readability of the report.

--- frappe_mpsa_payments/frappe_mpsa_payments/report/b2c_disbursement_summary/b2c_disbursement_summary.js
+++ frappe_mpsa_payments/frappe_mpsa_payments/report/b2c_disbursement_summary/b2c_disbursement_summary.js
@@ -32,4 +32,31 @@ frappe.query_reports['B2C Disbursement Summary'] = {
       options: '\nEmployee\nSupplier\nCustomer',
     },
   ],
+  formatter: function (value, row, column, data, default_formatter) {
+    let formatted_value = default_formatter(value, row, column, data);
+
+    if (data && data.is_subtotal) {
+      formatted_value = `<span style="font-weight: bold;">${formatted_value}</span>`;
+    }
+
+    const blankFields = ['total_amount'];
+
+    const isBlankableColumn =
+      blankFields.includes(column.fieldname) || /\d+$/.test(column.fieldname);
+
+    const shouldBlank =
+      data &&
+      (value === null ||
+        value === undefined ||
+        value === '' ||
+        value === 0 ||
+        value === '0%' ||
+        formatted_value === 'Sh 0.00');
+
+    if (isBlankableColumn && shouldBlank) {
+      return '';
+    }
+
+    return formatted_value;
+  },
 };
