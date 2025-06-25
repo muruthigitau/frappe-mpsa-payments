7ba38e423a27fef1a5f57565f016166f4fba18f4
symonmuchemi <simonkenya846@gmail.com>
2025-06-26 02:12:13 +0300

    feat: update B2C Disbursement Summary report columns and data retrieval logic
    - Removed the 'company' field from the columns definition in the get_columns function.
    - Added 'party_type' and 'party' fields to the columns definition for better reporting.
    - Refactored the get_data function to enhance data retrieval by applying filters directly to the disbursement query.
    - Improved the handling of references to include total amounts and currencies, ensuring accurate subtotal calculations for disbursements.
    - Introduced a new apply_disbursement_filters function to streamline the filtering process based on user inputs.

--- frappe_mpsa_payments/frappe_mpsa_payments/report/b2c_disbursement_summary/b2c_disbursement_summary.py
+++ frappe_mpsa_payments/frappe_mpsa_payments/report/b2c_disbursement_summary/b2c_disbursement_summary.py
@@ -27,13 +27,6 @@ def get_columns():
             "width": 250,
         },
         {
-            "fieldname": "company",
-            "fieldtype": "Link",
-            "label": "Company",
-            "options": "Company",
-            "width": 250,
-        },
-        {
             "fieldname": "mode_of_payment",
             "fieldtype": "Link",
             "label": "Mode of Payment",
@@ -45,6 +38,19 @@ def get_columns():
             "fieldtype": "Link",
             "label": "Transaction to Pay Against",
             "options": "DocType",
+            "width": 150,
+        },
+        {
+            "fieldname": "party_type",
+            "fieldtype": "Link",
+            "label": "Party Type",
+            "options": "Party Type",
+            "width": 100,
+        },
+        {
+            "fieldname": "party",
+            "fieldtype": "Data",
+            "label": "Party",
             "width": 150,
         },
         {
@@ -60,19 +66,6 @@ def get_columns():
             "label": "Amount",
             "width": 120,
         },
-        {
-            "fieldname": "party_type",
-            "fieldtype": "Link",
-            "label": "Party Type",
-            "options": "Party Type",
-            "width": 100,
-        },
-        {
-            "fieldname": "party",
-            "fieldtype": "Data",  # TODO: Change to Link if needed
-            "label": "Party",
-            "width": 150,
-        },
         {"fieldname": "status", "fieldtype": "Data", "label": "Status", "width": 100},
     ]
 
@@ -81,33 +74,104 @@ def get_data(filters):
     Disbursement = frappe.qb.DocType("B2C Payment Disbursement")
     Reference = frappe.qb.DocType("B2C Payment Disbursement Reference")
 
-    query = (
+    final_report_data = []
+
+    disbursement_query = (
         frappe.qb.from_(Disbursement)
-        .left_join(Reference)
-        .on(Disbursement.name == Reference.parent)
         .select(
-            Disbursement.name.as_("name"),
-            Disbursement.payment_type.as_("payment_type"),
-            Disbursement.posting_date.as_("posting_date"),
-            Disbursement.company.as_("company"),
-            Disbursement.mode_of_payment.as_("mode_of_payment"),
-            Disbursement.transaction_to_pay_against.as_("transaction_to_pay_against"),
-            Disbursement.status.as_("status"),
-            Reference.total_amount.as_("total_amount"),
-            Reference.currency.as_("currency"),
-            Reference.party.as_("party"),
-            Reference.party_type.as_("party_type"),
+            Disbursement.name,
+            Disbursement.posting_date,
+            Disbursement.company,
+            Disbursement.mode_of_payment,
+            Disbursement.transaction_to_pay_against,
+            Disbursement.status,
+            Disbursement.payment_type,
         )
+        .orderby(Disbursement.posting_date)
     )
 
-    query = apply_filters(query, filters, Disbursement, Reference)
+    disbursement_query = apply_disbursement_filters(
+        disbursement_query, filters, Disbursement
+    )
+    disbursements = disbursement_query.run(as_dict=True)
 
-    data = query.run(as_dict=True)
+    for disburse in disbursements:
 
-    return data
+        final_report_data.append(
+            {
+                "posting_date": disburse.posting_date,
+                "name": disburse.name,
+                "company": disburse.company,
+                "mode_of_payment": disburse.mode_of_payment,
+                "transaction_to_pay_against": disburse.transaction_to_pay_against,
+                "total_amount": None,
+                "currency": None,
+                "party_type": None,
+                "party": None,
+            }
+        )
+
+        references_query = (
+            frappe.qb.from_(Reference)
+            .select(
+                Reference.total_amount,
+                Reference.currency,
+                Reference.party,
+                Reference.party_type,
+            )
+            .where(Reference.parent == disburse.name)
+        )
+
+        if filters.get("party_type"):
+            references_query = references_query.where(
+                Reference.party_type == filters["party_type"]
+            )
+
+        references = references_query.run(as_dict=True)
+
+        current_disbursement_subtotal = 0.0
+        for ref in references:
+            final_report_data.append(
+                {
+                    "posting_date": ref.posting_date if "posting_date" in ref else None,
+                    "name": disburse.name,
+                    "company": disburse.company,
+                    "mode_of_payment": disburse.mode_of_payment,
+                    "transaction_to_pay_against": (disburse.transaction_to_pay_against),
+                    "status": ref.status if "status" in ref else disburse.status,
+                    "total_amount": ref.total_amount,
+                    "currency": ref.currency,
+                    "party_type": ref.party_type,
+                    "party": ref.party,
+                }
+            )
+            if ref.total_amount is not None:
+                current_disbursement_subtotal += float(ref.total_amount)
+
+        final_report_data.append(
+            {
+                "is_subtotal": True,
+                "posting_date": None,
+                "name": f"{disburse.name}",
+                "company": None,
+                "mode_of_payment": None,
+                "transaction_to_pay_against": None,
+                "currency": None,
+                "total_amount": current_disbursement_subtotal,
+                "party_type": None,
+                "party": None,
+                "status": None,
+                "bold": True,
+            }
+        )
+
+        final_report_data.append({})
+
+    return final_report_data
 
 
-def apply_filters(query, filters, Disbursement, Reference):
+def apply_disbursement_filters(query, filters, Disbursement):
+    """Applies filters relevant to the B2C Payment Disbursement doctype."""
     if filters:
         if filters.get("start_date"):
             query = query.where(Disbursement.posting_date >= filters["start_date"])
@@ -115,6 +179,5 @@ def apply_filters(query, filters, Disbursement, Reference):
             query = query.where(Disbursement.posting_date <= filters["end_date"])
         if filters.get("company"):
             query = query.where(Disbursement.company == filters["company"])
-        if filters.get("party_type"):
-            query = query.where(Reference.party_type == filters["party_type"])
+
     return query
