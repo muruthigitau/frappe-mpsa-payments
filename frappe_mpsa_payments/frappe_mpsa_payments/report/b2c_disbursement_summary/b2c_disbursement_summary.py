d4c73358d5e08c8e1f035b106a442e5c49c84da7
symonmuchemi <simonkenya846@gmail.com>
2025-06-26 01:18:44 +0300

    feat: implement data retrieval and column definition for B2C Disbursement Summary report
    - Updated the execute function to retrieve columns and data based on provided filters.
    - Defined the get_columns function to specify the structure of the report, including fields such as posting date, disbursement, company, mode of payment, transaction details, currency, total amount, party, and party type.

--- frappe_mpsa_payments/frappe_mpsa_payments/report/b2c_disbursement_summary/b2c_disbursement_summary.py
+++ frappe_mpsa_payments/frappe_mpsa_payments/report/b2c_disbursement_summary/b2c_disbursement_summary.py
@@ -5,5 +5,71 @@
 
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    columns = get_columns()
+    columns = get_data(filters)
+
+    return columns, data
+
+
+def get_columns():
+    return [
+        {
+            "fieldname": "posting_date",
+            "fieldtype": "Date",
+            "label": "Posting Date",
+            "width": 120,
+        },
+        {
+            "fieldname": "name",
+            "fieldtype": "Link",
+            "label": "Disbursement",
+            "options": "B2C Payment Disbursement",
+            "width": 150,
+        },
+        {
+            "fieldname": "company",
+            "fieldtype": "Link",
+            "label": "Company",
+            "options": "Company",
+            "width": 150,
+        },
+        {
+            "fieldname": "mode_of_payment",
+            "fieldtype": "Link",
+            "label": "Mode of Payment",
+            "options": "Mode of Payment",
+            "width": 150,
+        },
+        {
+            "fieldname": "transaction_to_pay_against",
+            "fieldtype": "Link",
+            "label": "Transaction to Pay Against",
+            "options": "DocType",
+            "width": 150,
+        },
+        {
+            "fieldname": "currency",
+            "fieldtype": "Link",
+            "label": "Currency",
+            "options": "Currency",
+            "width": 100,
+        },
+        {
+            "fieldname": "total_amount",
+            "fieldtype": "Currency",
+            "label": "Amount",
+            "width": 120,
+        },
+        {
+            "fieldname": "party",
+            "fieldtype": "Data",  # TODO: Change to Link if needed
+            "label": "Party",
+            "width": 150,
+        },
+        {
+			"fieldname": "party_type",
+			"fieldtype": "Data",  # TODO: Change to Link if needed
+			"label": "Party Type",
+			"width": 150,
+		}
+    ]
