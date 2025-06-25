1dd6520fdd5958ef089377e4c7e2b5666435281c
symonmuchemi <simonkenya846@gmail.com>
2025-06-26 00:52:45 +0300

    chore: add B2C Disbursement Summary report files
    - Created new report files for the B2C Disbursement Summary, including the initialization file, JavaScript configuration, JSON metadata, and Python execution logic.
    - The report is designed to provide a summary of B2C payment disbursements and to track outgoing funds .

---
+++ frappe_mpsa_payments/frappe_mpsa_payments/report/b2c_disbursement_summary/b2c_disbursement_summary.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Navari Limited and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["B2C Disbursement Summary"] = {
+	"filters": [
+
+	]
+};
